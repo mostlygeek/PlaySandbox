9421f99ea79525039d9ac21caa215b9d31e0981a
Benson Wong <mostlygeek@gmail.com>
2012-05-02 22:32:17 -0700

    reformatted ugly

--- app/controllers/Benchmark.scala
+++ app/controllers/Benchmark.scala
@@ -3,57 +3,57 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.libs._
-import play.api.libs._		 // mostly for the concurrent library
+import play.api.libs._ // mostly for the concurrent library
 import play.api.Play.current // required for Akka.future
 
 /**
- * This tests if Play is a singled thread process handling requests 
- * or if it is multi-threaded, handling each request separately. 
- * 
- * See below for the ab (apache bench) results. 
- * 
+ * This tests if Play is a singled thread process handling requests
+ * or if it is multi-threaded, handling each request separately.
+ *
+ * See below for the ab (apache bench) results.
+ *
  */
 object Benchmark extends Controller {
-	
-	def fast = Action {
-	  Ok("Hi")
-	}
-	
-	def block = Action {
-	  val now = System.currentTimeMillis()
-	  val sleepFor:Long = 1000
-	  Thread.sleep(sleepFor)
-	    
-	  Ok("Done, blocked for: " + sleepFor + "ms")
-	}
-	
-	/**
-	 * Ref: http://www.playframework.org/documentation/2.0/ScalaAsync
-	 * 
-	 * Play basically expects requests to finish as fast as possible, 
-	 * like a single threaded application.
-	 */
-	
-	def async = Action {
-		val sleepFor:Long = 1000
-		Async {		  
-		  val p1 : concurrent.Promise[Long] = concurrent.Akka.future {
-		    // usually put a long running function here that returns a value
-		    // when it is done... 
-		    Thread.sleep(sleepFor) 
-		    sleepFor
-		    
-		    /* using Thread.sleep is bad, it is good to use the Actor system
+
+    def fast = Action {
+        Ok("Hi")
+    }
+
+    def block = Action {
+        val now = System.currentTimeMillis()
+        val sleepFor: Long = 1000
+        Thread.sleep(sleepFor)
+
+        Ok("Done, blocked for: " + sleepFor + "ms")
+    }
+
+    /**
+     * Ref: http://www.playframework.org/documentation/2.0/ScalaAsync
+     *
+     * Play basically expects requests to finish as fast as possible,
+     * like a single threaded application.
+     */
+
+    def async = Action {
+        val sleepFor: Long = 1000
+        Async {
+            val p1: concurrent.Promise[Long] = concurrent.Akka.future {
+                // usually put a long running function here that returns a value
+                // when it is done... 
+                Thread.sleep(sleepFor)
+                sleepFor
+
+                /* using Thread.sleep is bad, it is good to use the Actor system
 		     * maybe on a remote node to do the long processing operation ... 
 		     * mmm
 		     */
-		  }
-		  
-		  p1.map { s => Ok("I async slept for: " + s + "ms") }
-		}
-	}
-	
-	/*
+            }
+
+            p1.map { s => Ok("I async slept for: " + s + "ms") }
+        }
+    }
+
+    /*
 	 * Instead of a long running function we use Actors, see the lib/Timers.scala
 	 * that will timeout in a specific amount of time and run our callback
 	 * function
@@ -66,14 +66,14 @@ object Benchmark extends Controller {
 	 * -- should take 2 seconds
 	 * 
 	 */
-	def async2 = Action {
-	  val redeemTime:Long = 1000 // ms
-	  Async {
-		  val prom = concurrent.Promise[Int]
-		  lib.Timers.in(redeemTime) {
-		    prom.redeem(1)
-		  }
-		  prom.map { _ => Ok("Reemed in: " + redeemTime + "ms") }
-	  }
-	}
+    def async2 = Action {
+        val redeemTime: Long = 1000 // ms
+        Async {
+            val prom = concurrent.Promise[Int]
+            lib.Timers.in(redeemTime) {
+                prom.redeem(1)
+            }
+            prom.map { _ => Ok("Reemed in: " + redeemTime + "ms") }
+        }
+    }
 }
