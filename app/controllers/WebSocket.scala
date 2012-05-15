f06120514253d552f9dc398c92ac1224d32ff210
Benson Wong <mostlygeek@gmail.com>
2012-05-14 20:55:28 -0700

    tweak WebSocket example to use an Enumerator that outputs every second

--- app/controllers/WebSocket.scala
+++ app/controllers/WebSocket.scala
@@ -1,17 +1,25 @@
 package controllers
 
+import java.util.Date
 import play.api._
 import play.api.mvc._
 import play.api.libs.iteratee.{Iteratee, Enumerator} // they are here :)
 import play.api.libs.ws.WS
-
+import play.api.libs.concurrent._
 
 // point browser @ http://www.websocket.org/echo.html to test it out
 // http://www.playframework.org/documentation/2.0.1/Iteratees
 object WebSocket extends Controller {
 
     def endpoint = play.api.mvc.WebSocket.using[String] { request => 
-        val out = Enumerator("Hello!")
+       
+        /**
+         * This will create an Enumerator that prints out the 
+         * time every second... 
+         */
+        val out = Enumerator.fromCallback { () =>
+        	Promise.timeout(Some(new Date().toString()), 1000)
+    	}
         
         val in = Iteratee.foreach[String] ( s => {                        
             println(s)
